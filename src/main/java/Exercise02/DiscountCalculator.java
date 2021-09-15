9848dca867ab0c21b18db4fddd6268d3525a5640
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-15 18:39:35 +0200

    goldenVIP toegevoegd

--- src/main/java/Exercise02/DiscountCalculator.java
+++ src/main/java/Exercise02/DiscountCalculator.java
@@ -3,25 +3,42 @@ package Exercise02;
 public class DiscountCalculator {
     private boolean isHappyHour;
     private boolean isVIP;
+    private boolean isGoldenVIP;
 
     public int calculatePrice(int price, int amount) {
         if (price<=0) return 0;
         double total;
-        if (amount<10) {
-            total = price * amount;
-            if (isVIP) total *= 0.9;
-        } else if (amount<20) {
-            total = 9 * price;
-            if (isVIP) total *= 0.9;
-            total = total + (amount - 9) * 0.9 * price;
-        } else {
-            total = 9 * price;
-            if (isVIP) total *= 0.9;
-            total = total + (amount - 19) * 0.8 * price + 10 * 0.9 * price;
+        total = price * (amount<10 ? amount : 9);
+        if (isGoldenVIP) {
+            total *= 0.8;
+        } else if (isVIP) {
+            total *= 0.9;
+        }
+        amount -= 9;
+        if (amount>0) {
+            double extra;
+            if (isGoldenVIP) {
+                extra = price * 0.8 * (amount<11 ? amount : 10);
+            } else {
+                extra = price * 0.9 * (amount<11 ? amount : 10);
+            }
+            total += extra;
+            amount -= 10;
+        }
+        if (amount>0) {
+            double extra;
+            if (isGoldenVIP) {
+                extra = price * 0.7 * (amount<31 ? amount : 30);
+            } else {
+                extra = price * 0.8 * amount;
+            }
+            amount -= 30;
+            total += extra;
+        }
+        if (amount>0 && isGoldenVIP) {
+            total += price * 0.5 * amount;
         }
         if (isHappyHour) total = total/2;
-        isHappyHour = false;
-        isVIP = false;
         return (int) Math.floor(total);
     }
 
@@ -31,5 +48,10 @@ public class DiscountCalculator {
 
     public void setVIPCustomer(boolean isVIP) {
         this.isVIP = isVIP;
+    }
+
+    public void setGoldenVIPCustomer(boolean isGolden) {
+        this.isGoldenVIP = isGolden;
+        if (this.isGoldenVIP) this.isVIP = true;
     }
 }
