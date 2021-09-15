e763697ef5b0495bedfce5b53d3a3b6fe64eba69
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-09-15 15:19:38 +0200

    code and tests for exercise 3.8-3.9 DiscountCalculator VIPs

---
+++ src/test/java/Exercise02/DiscountCalculatorGoldenVIPCustomerTest.java
@@ -0,0 +1,72 @@
+package Exercise02;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DiscountCalculatorGoldenVIPCustomerTest {
+/*
+
+    private final DiscountCalculator calc = new DiscountCalculator();
+
+    @BeforeEach
+    public void activateSpecialConditions() {
+        calc.setGoldenVIPCustomer(true);
+    }
+
+    @Test
+    public void noPrice() {
+        int result = calc.calculatePrice(0, 10);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void amount0() {
+        int result = calc.calculatePrice(10, 0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void amount1() {
+        int result = calc.calculatePrice(10, 1);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void amount10() {
+        int result = calc.calculatePrice(10, 10);
+        assertEquals(80, result);
+    }
+
+    @Test
+    public void amount11() {
+        int result = calc.calculatePrice(10, 11);
+        assertEquals(88, result);
+    }
+
+    @Test
+    public void amount20() {
+        int result = calc.calculatePrice(10, 20);
+        assertEquals(159, result);
+    }
+
+    @Test
+    public void amount50() {
+        int result = calc.calculatePrice(10, 50);
+        assertEquals(367, result);
+    }
+
+    @Test
+    public void amount50AndPrice1() {
+        int result = calc.calculatePrice(1, 50);
+        assertEquals(36, result);
+    }
+
+    @Test
+    public void amount60() {
+        int result = calc.calculatePrice(10, 60);
+        assertEquals(417, result);
+    }
+*/
+}
