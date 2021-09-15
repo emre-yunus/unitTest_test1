edad6e3147f8cf738acdd1dc2b36fef3b1d5f746
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-09-15 15:30:03 +0200

    code and tests for exercise 4.3: Product

---
+++ src/main/java/Exercise04/Product.java
@@ -0,0 +1,50 @@
+package Exercise04;
+
+public class Product {
+    private String name;
+    private Integer size;
+    private Double percentage;
+
+    public Product(String name, Integer size, Double percentage) {
+        this.name = name;
+        this.size = size;
+        this.percentage = percentage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Double percentage) {
+        this.percentage = percentage;
+    }
+
+    public String productDescription() {
+        if (size == null && percentage == null) return "";
+        String result = " • ";
+        if (size != null)
+            result += size + "CL";
+        if (percentage != null) {
+            String format = (percentage % 1 == 0) ? "%.0f%%" : "%.1f%%";
+            if (size != null) result += " ";
+            result += String.format(format, percentage);
+        }
+        return result;
+    }
+}
