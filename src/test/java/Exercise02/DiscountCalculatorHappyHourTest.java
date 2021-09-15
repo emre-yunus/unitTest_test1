40ac1e62109db2b0be6101d0738deffcbc9b6d34
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-09-15 15:14:37 +0200

    code and tests for exercise 3.6 DiscountCalculator HappyHour

---
+++ src/test/java/Exercise02/DiscountCalculatorHappyHourTest.java
@@ -0,0 +1,65 @@
+package Exercise02;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DiscountCalculatorHappyHourTest {
+/*
+    private final DiscountCalculator calc = new DiscountCalculator();
+
+    @BeforeEach
+    public void activateSpecialConditions() {
+        calc.setHappyHour(true);
+    }
+
+    @Test
+    public void noPrice() {
+        int result = calc.calculatePrice(0, 10);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void amount0() {
+        int result = calc.calculatePrice(10, 0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void amount1() {
+        int result = calc.calculatePrice(10, 1);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void amount10() {
+        int result = calc.calculatePrice(10, 10);
+        assertEquals(49, result);
+    }
+
+    @Test
+    public void amount11() {
+        int result = calc.calculatePrice(10, 11);
+        assertEquals(54, result);
+    }
+
+    @Test
+    public void amount20() {
+        int result = calc.calculatePrice(10, 20);
+        assertEquals(94, result);
+    }
+
+    @Test
+    public void amount50() {
+        int result = calc.calculatePrice(10, 50);
+        assertEquals(214, result);
+    }
+
+    @Test
+    public void amount50AndPrice1() {
+        int result = calc.calculatePrice(1, 50);
+        assertEquals(21, result);
+    }
+*/
+}
