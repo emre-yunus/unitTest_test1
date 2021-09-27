de085bc7bf0ba82654619dc1c66468a03bbc7426
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-27 10:24:09 +0200

    Ingredient uitproberen: achterhalen code

--- src/main/java/Exercise03/Ingredient.java
+++ src/main/java/Exercise03/Ingredient.java
@@ -37,22 +37,31 @@ public class Ingredient {
 
     public static void main(String[] args) {
         Ingredient i1 = new Ingredient(1, "appel", "appels");
-        System.out.println(i1.print(4));
+        System.out.println(i1.print(1));
         Ingredient i2 = new Ingredient(2, "appel", "appels");
-        System.out.println(i2.print(4));
-        Ingredient i3 = new Ingredient(100, "gram", "gram", "bloem");
-        System.out.println(i3.print(3));
-        Ingredient i4 = new Ingredient(0, "gram", "gram", "bloem");
-        System.out.println(i4.print(3));
-        Ingredient i5 = new Ingredient(1, "kopje", "kopjes", "suiker");
-        System.out.println(i5.print(2));
-        Ingredient i6 = new Ingredient(2, "kopje", "kopjes", "suiker");
-        System.out.println(i6.print(2));
-        Ingredient i7 = new Ingredient(1, "eetlepel", "eetlepels", "suiker");
-        System.out.println(i7.print(4));
-        Ingredient i8 = new Ingredient("peper en zout");
-        System.out.println(i8.print(4));
-        Ingredient i9 = new Ingredient("peper en zout");
-        System.out.println(i9.print(17));
+        System.out.println(i2.print(1));
+        Ingredient i3 = new Ingredient(2, "appel", "appels");
+        System.out.println(i3.print(10));
+
+        Ingredient i4 = new Ingredient(100, "gram", "gram", "bloem");
+        System.out.println(i4.print(0));
+        Ingredient i5 = new Ingredient(0, "gram", "gram", "bloem");
+        System.out.println(i5.print(10));
+        Ingredient i6 = new Ingredient(100, "gram", "gram", "bloem");
+        System.out.println(i6.print(1));
+        Ingredient i7 = new Ingredient(100, "gram", "gram", "bloem");
+        System.out.println(i7.print(2));
+
+        Ingredient i8 = new Ingredient(1, "eetlepel", "eetlepels", "suiker");
+        System.out.println(i8.print(20));
+        Ingredient i9 = new Ingredient(2, "eetlepel", "eetlepels", "suiker");
+        System.out.println(i9.print(2));
+        Ingredient i10 = new Ingredient(1, "eetlepel", "eetlepels", "suiker");
+        System.out.println(i10.print(0));
+
+        Ingredient i11 = new Ingredient("peper en zout");
+        System.out.println(i11.print(0));
+        Ingredient i12 = new Ingredient("peper en zout");
+        System.out.println(i12.print(17));
     }
 }
