fb8913dffbe4e941fc1d7d544731e2c6536fa466
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-27 10:32:29 +0200

    Recipe uitproberen: achterhalen code

--- src/main/java/Exercise03/Recipe.java
+++ src/main/java/Exercise03/Recipe.java
@@ -24,7 +24,7 @@ public class Recipe {
         if (nrOfPersons == 0) return result;
 
         for (Ingredient ingredient : ingredients) {
-            result += "\n" + ingredient.print(nrOfPersons / amountForRecipe);
+            result += "\n  " + ingredient.print(nrOfPersons / amountForRecipe);
         }
         return result;
 
@@ -35,6 +35,44 @@ public class Recipe {
     }
 
     public static void main(String[] args) {
+        Recipe r1 = new Recipe("appeltaart");
+        System.out.println(r1.print(0));
 
+        Recipe r2 = new Recipe("appeltaart");
+        System.out.println(r2.print(1));
+
+        Recipe r3 = new Recipe("appeltaart");
+        r3.addIngredient(new Ingredient(1, "kopje", "kopjes", "bloem"));
+        System.out.println(r3.print(0));
+
+        Recipe r4 = new Recipe("appeltaart");
+        r4.addIngredient(new Ingredient(1, "kopje", "kopjes", "bloem"));
+        System.out.println(r4.print(1));
+
+        Recipe r5 = new Recipe("appeltaart");
+        r5.addIngredient(new Ingredient(1, "kopje", "kopjes", "bloem"));
+        r5.addIngredient(new Ingredient(2, "eetlepel", "eetlepels", "suiker"));
+        System.out.println(r5.print(1));
+
+        Recipe r6 = new Recipe("appeltaart");
+        r6.addIngredient(new Ingredient(1, "kopje", "kopjes", "bloem"));
+        r6.addIngredient(new Ingredient(2, "eetlepel", "eetlepels", "suiker"));
+        System.out.println(r6.print(10));
+
+        Recipe r7 = new Recipe("cake", 4);
+        r7.addIngredient(new Ingredient(1, "kopje", "kopjes", "bloem"));
+        r7.addIngredient(new Ingredient(2, "eetlepel", "eetlepels", "suiker"));
+        System.out.println(r7.print(4));
+
+        Recipe r8 = new Recipe("cake", 4);
+        r8.addIngredient(new Ingredient(1, "kopje", "kopjes", "bloem"));
+        r8.addIngredient(new Ingredient(2, "eetlepel", "eetlepels", "suiker"));
+        System.out.println(r8.print(8));
+
+        Recipe r9 = new Recipe("cake", 4);
+        r9.addIngredient(new Ingredient(1, "kopje", "kopjes", "bloem"));
+        r9.addIngredient(new Ingredient(2, "eetlepel", "eetlepels", "suiker"));
+        r9.addIngredient(new Ingredient("citroensap"));
+        System.out.println(r9.print(4));
     }
 }
--- src/main/java/Exercise04/Product.java
+++ src/main/java/Exercise04/Product.java
@@ -49,4 +49,6 @@ public class Product {
         }
         return result;
     }
+
+    
 }
