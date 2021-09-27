b10b661132daf59839945b12c66fb16d638d839d
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-27 15:47:26 +0200

    testen ProductLocale toegevoegd

--- src/test/java/Exercise04/ProductLocaleTest.java
+++ src/test/java/Exercise04/ProductLocaleTest.java
@@ -1,4 +1,38 @@
 package Exercise04;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ProductLocaleTest {
+    Locale originalLocale;
+
+    @BeforeEach
+    void setup() {
+        originalLocale = Locale.getDefault();
+        Locale.setDefault(Locale.FRANCE);
+    }
+
+    @AfterEach
+    void teardown() {
+        Locale.setDefault(originalLocale);
+    }
+
+    @Test
+    void commonElements_percentageWithDecimal() {
+        Product p = new Product("empty", 33, 6.3);
+        String result = p.productDescription();
+        assertEquals(" • 33CL 6.3%", result);
+    }
+
+    @Test
+    void commonElements_percentageWithTooMuchDecimals() {
+        Product p = new Product("empty", 33, 6.333);
+        String result = p.productDescription();
+        assertEquals(" • 33CL 6.3%", result);
+    }
 }
